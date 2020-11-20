26ce6b502f35e7a5b7a9185888a10c3d02cf22a5
tabishhameed <tabishhameed34@gmail.com>
2020-11-20 18:17:22 +0800

    git commit -m "first commit" git remote add origin https://github.com/tabishhameed/qr-code.git
    git change the file name

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import png
 from pyqrcode import QRCode
 str = "www.youtube.com"
 url = pyqrcode.create(str)
-url.png("tabish.png", scale=20)
+url.png("aziz.png", scale=20)
 #url.svg("my.svg",scale=20)
